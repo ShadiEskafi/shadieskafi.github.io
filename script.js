aaf69d8d000421655de73d9a98713d3ba61bd930
ShadiEskafi <shadieskafi@gmail.com>
2025-07-21 14:15:27 +0300

    النسخة التجريبية من موقعي الشخصي

---
+++ script.js
@@ -0,0 +1,17 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+});
+
+// التعامل مع الضغط على روابط التنقل
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      scroll.scrollTo(target);
+    }
+  });
+});
+
+document.getElementById('year').textContent = new Date().getFullYear();
